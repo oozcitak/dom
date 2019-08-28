78d7f9eb315dc93ff6f7deca99f0cfa578760416
Ozgur Ozcitak <ozcitak@yahoo.com>
2019-08-28 15:46:14 +0300

    Add dom type guard tests

---
+++ test/dom/util/Guard.test.ts
@@ -0,0 +1,50 @@
+import $$ from './TestHelpers'
+
+describe('Guard', function () {
+
+  const doc = $$.dom.createDocument('ns', 'root')
+
+  if (!doc.documentElement)
+    throw new Error("documentElement is null")
+
+  test('document type guards', function () {
+    expect($$.Guard.isNode(doc)).toBeTruthy()
+    
+    expect($$.Guard.isDocumentNode(doc)).toBeTruthy()
+    expect($$.Guard.isDocumentTypeNode($$.dom.createDocumentType('root', 'pub', 'sys'))).toBeTruthy()
+    expect($$.Guard.isDocumentFragmentNode(doc.createDocumentFragment())).toBeTruthy()
+  })
+
+  test('element and attribute type guards', function () {
+    expect($$.Guard.isElementNode(doc.createElement('ele'))).toBeTruthy()
+    expect($$.Guard.isAttrNode(doc.createAttribute('attr'))).toBeTruthy()
+  })
+
+  test('character data type guards', function () {
+    expect($$.Guard.isCharacterDataNode(doc.createTextNode('value'))).toBeTruthy()
+    expect($$.Guard.isCharacterDataNode(doc.createProcessingInstruction('target', 'value'))).toBeTruthy()
+    expect($$.Guard.isCharacterDataNode(doc.createComment('value'))).toBeTruthy()
+    expect($$.Guard.isCharacterDataNode(doc.createCDATASection('value'))).toBeTruthy()
+  })
+
+  test('text type guards', function () {
+    expect($$.Guard.isTextNode(doc.createTextNode('value'))).toBeTruthy()
+    expect($$.Guard.isTextNode(doc.createProcessingInstruction('target', 'value'))).toBeFalsy()
+    expect($$.Guard.isTextNode(doc.createComment('value'))).toBeFalsy()
+    expect($$.Guard.isTextNode(doc.createCDATASection('value'))).toBeTruthy()
+  }) 
+
+  test('exclusive text type guards', function () {
+    expect($$.Guard.isExclusiveTextNode(doc.createTextNode('value'))).toBeTruthy()
+    expect($$.Guard.isExclusiveTextNode(doc.createProcessingInstruction('target', 'value'))).toBeFalsy()
+    expect($$.Guard.isExclusiveTextNode(doc.createComment('value'))).toBeFalsy()
+    expect($$.Guard.isExclusiveTextNode(doc.createCDATASection('value'))).toBeFalsy()
+  }) 
+
+  test('specific character data type guards', function () {
+    expect($$.Guard.isProcessingInstructionNode(doc.createProcessingInstruction('target', 'value'))).toBeTruthy()
+    expect($$.Guard.isCommentNode(doc.createComment('value'))).toBeTruthy()
+    expect($$.Guard.isCDATASectionNode(doc.createCDATASection('value'))).toBeTruthy()
+  }) 
+
+})
---
+++ test/dom/util/TestHelpers.ts
@@ -0,0 +1,7 @@
+import TestHelpersRoot from "../TestHelpers"
+import { Cast, Guard } from '../../../src/dom/util'
+
+export default class TestHelpers extends TestHelpersRoot {
+  static Cast = Cast
+  static Guard = Guard
+}
