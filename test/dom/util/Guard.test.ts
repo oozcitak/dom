ad9155f1dbdea7b64ba7967ea162b8fab1ac0e76
Ozgur Ozcitak <ozcitak@yahoo.com>
2019-09-17 18:05:27 +0300

    Add null guard checks

--- test/dom/util/Guard.test.ts
+++ test/dom/util/Guard.test.ts
@@ -7,9 +7,26 @@ describe('Guard', function () {
   if (!doc.documentElement)
     throw new Error("documentElement is null")
 
+  test('null checks', function () {
+    expect($$.Guard.isNode(null)).toBeFalsy()
+    expect($$.Guard.isDocumentNode(null)).toBeFalsy()
+    expect($$.Guard.isDocumentTypeNode(null)).toBeFalsy()
+    expect($$.Guard.isDocumentFragmentNode(null)).toBeFalsy()
+    expect($$.Guard.isElementNode(null)).toBeFalsy()
+    expect($$.Guard.isAttrNode(null)).toBeFalsy()
+    expect($$.Guard.isCharacterDataNode(null)).toBeFalsy()
+    expect($$.Guard.isTextNode(null)).toBeFalsy()
+    expect($$.Guard.isExclusiveTextNode(null)).toBeFalsy()
+    expect($$.Guard.isProcessingInstructionNode(null)).toBeFalsy()
+    expect($$.Guard.isCommentNode(null)).toBeFalsy()
+    expect($$.Guard.isCDATASectionNode(null)).toBeFalsy()
+    expect($$.Guard.isRegisteredObserver(null)).toBeFalsy()
+    expect($$.Guard.isTransientRegisteredObserver(null)).toBeFalsy()
+  })
+
   test('document type guards', function () {
     expect($$.Guard.isNode(doc)).toBeTruthy()
-    
+
     expect($$.Guard.isDocumentNode(doc)).toBeTruthy()
     expect($$.Guard.isDocumentTypeNode($$.dom.createDocumentType('root', 'pub', 'sys'))).toBeTruthy()
     expect($$.Guard.isDocumentFragmentNode(doc.createDocumentFragment())).toBeTruthy()
@@ -32,19 +49,19 @@ describe('Guard', function () {
     expect($$.Guard.isTextNode(doc.createProcessingInstruction('target', 'value'))).toBeFalsy()
     expect($$.Guard.isTextNode(doc.createComment('value'))).toBeFalsy()
     expect($$.Guard.isTextNode(doc.createCDATASection('value'))).toBeTruthy()
-  }) 
+  })
 
   test('exclusive text type guards', function () {
     expect($$.Guard.isExclusiveTextNode(doc.createTextNode('value'))).toBeTruthy()
     expect($$.Guard.isExclusiveTextNode(doc.createProcessingInstruction('target', 'value'))).toBeFalsy()
     expect($$.Guard.isExclusiveTextNode(doc.createComment('value'))).toBeFalsy()
     expect($$.Guard.isExclusiveTextNode(doc.createCDATASection('value'))).toBeFalsy()
-  }) 
+  })
 
   test('specific character data type guards', function () {
     expect($$.Guard.isProcessingInstructionNode(doc.createProcessingInstruction('target', 'value'))).toBeTruthy()
     expect($$.Guard.isCommentNode(doc.createComment('value'))).toBeTruthy()
     expect($$.Guard.isCDATASectionNode(doc.createCDATASection('value'))).toBeTruthy()
-  }) 
+  })
 
 })
