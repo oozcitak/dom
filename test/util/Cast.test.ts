93460614970420a12b04147219e59bcb1bdb641f
Ozgur Ozcitak <ozcitak@yahoo.com>
2019-11-04 13:00:56 +0300

    Add Cast tests

---
+++ test/util/Cast.test.ts
@@ -0,0 +1,12 @@
+import $$ from '../TestHelpers'
+
+describe('Cast', () => {
+
+  const ele = $$.newDoc
+
+  test('asNode()', () => {
+    expect($$.util.Cast.asNode(ele)).toBe(ele)
+    expect(() => $$.util.Cast.asNode("not a node")).toThrow()
+  })
+
+})
