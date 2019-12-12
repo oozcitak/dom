f50f0dd0c6145a29478e332d9be5fb5bb0331d56
Ozgur Ozcitak <ozcitak@yahoo.com>
2019-12-12 12:14:05 +0300

    Add more nodes to performance test case

--- perf/perf.ts
+++ perf/perf.ts
@@ -13,6 +13,12 @@ import { processBenchmark } from "./"
     doc.appendChild(root)
     for (let i = 0; i < 100; i++) {
       const node = doc.createElement("node" + i.toString())
+      for (let j = 0; j < 10; j++) {
+        node.setAttribute("att" + j.toString(), "val" + j.toString())
+        const text = doc.createTextNode("text" + j.toString())
+        node.appendChild(text)
+      }
+
       root.appendChild(node)
     }
   }
