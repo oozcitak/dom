78cb84e983a073dfbcf7f988a42f7b7ac8130ec5
Ozgur Ozcitak <ozcitak@yahoo.com>
2019-12-23 12:45:04 +0300

    Lazy initialize regexps

--- src/algorithm/XMLAlgorithm.ts
+++ src/algorithm/XMLAlgorithm.ts
@@ -1,44 +1,4 @@
-const Name = /^([:A-Z_a-z\xC0-\xD6\xD8-\xF6\xF8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])([\x2D\.0-:A-Z_a-z\xB7\xC0-\xD6\xD8-\xF6\xF8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])*$/
-const QName = /^(([A-Z_a-z\xC0-\xD6\xD8-\xF6\xF8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])([\x2D\.0-9A-Z_a-z\xB7\xC0-\xD6\xD8-\xF6\xF8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])*:([A-Z_a-z\xC0-\xD6\xD8-\xF6\xF8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])([\x2D\.0-9A-Z_a-z\xB7\xC0-\xD6\xD8-\xF6\xF8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])*)|(([A-Z_a-z\xC0-\xD6\xD8-\xF6\xF8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])([\x2D\.0-9A-Z_a-z\xB7\xC0-\xD6\xD8-\xF6\xF8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])*)$/
-const PubidChar = /^[\x20\x0D\x0AA-Z-a-z0-9-'()+,./:=?;!*#@$_%]*$/
-
-/** 
- * Valid characters for XML 1.0 from https://www.w3.org/TR/xml/#charsets.
- * 
- * Any Unicode character, excluding the surrogate blocks, `FFFE`, and `FFFF`.
- * 
- * `#x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]`
- * 
- * This ES5 compatible Regexp has been generated using the `"regenerate"` NPM
- * module:
- * ```ts
- * let xml_10_InvalidChars = regenerate()
- *   .addRange(0x0000, 0x0008)
- *   .add(0x000B, 0x000C)
- *   .addRange(0x000E, 0x001F)
- *   .addRange(0xD800, 0xDFFF)
- *   .addRange(0xFFFE, 0xFFFF)
- * ```
- */
-const InvalidChar_10 = /[\0-\x08\x0B\f\x0E-\x1F\uFFFE\uFFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]/
-
-/**
- * Valid characters for XML 1.1 from https://www.w3.org/TR/xml11/#charsets.
- * 
- * Any Unicode character, excluding the surrogate blocks, `FFFE`, and `FFFF`.
- * 
- * `[#x1-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]`
- * 
- * This ES5 compatible Regexp has been generated using the `"regenerate"` NPM
- * module:
- * ```ts
- * let xml_11_InvalidChars = regenerate()
- *   .add(0x0000)
- *   .addRange(0xD800, 0xDFFF)
- *   .addRange(0xFFFE, 0xFFFF)
- * ```
- */
-const InvalidChar_11 = /[\0\uFFFE\uFFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]/
+import { DOMImpl } from "../dom/DOMImpl"
 
 /**
  * Determines if the given string is valid for a `"Name"` construct.
@@ -46,7 +6,7 @@ const InvalidChar_11 = /[\0\uFFFE\uFFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[
  * @param name - name string to test
  */
 export function xml_isName(name: string): boolean {
-  return Name.test(name)
+  return DOMImpl.instance.regExp.Name.value.test(name)
 }
 
 /**
@@ -55,7 +15,7 @@ export function xml_isName(name: string): boolean {
  * @param name - name string to test
  */
 export function xml_isQName(name: string): boolean {
-  return QName.test(name)
+  return DOMImpl.instance.regExp.QName.value.test(name)
 }
 
 /**
@@ -66,9 +26,9 @@ export function xml_isQName(name: string): boolean {
  */
 export function xml_isLegalChar(chars: string, xmlVersion: "1.0" | "1.1" = "1.0"): boolean {
   if (xmlVersion === "1.0") {
-    return (!InvalidChar_10.test(chars))
+    return (!DOMImpl.instance.regExp.InvalidChar_10.value.test(chars))
   } else {
-    return (!InvalidChar_11.test(chars))
+    return (!DOMImpl.instance.regExp.InvalidChar_11.value.test(chars))
   }
 }
 
@@ -80,5 +40,5 @@ export function xml_isLegalChar(chars: string, xmlVersion: "1.0" | "1.1" = "1.0"
  * @param chars - sequence of characters to test
  */
 export function xml_isPubidChar(chars: string): boolean {
-  return PubidChar.test(chars)
+  return DOMImpl.instance.regExp.PubidChar.value.test(chars)
 }
--- src/dom/DOMImpl.ts
+++ src/dom/DOMImpl.ts
@@ -1,6 +1,14 @@
 import { DOMFeatures, Window, Node, Range } from "./interfaces"
-import { CompareCache, ObjectCache, isObject } from "@oozcitak/util"
+import { CompareCache, ObjectCache, Lazy, isObject } from "@oozcitak/util"
 import { create_window } from "../algorithm"
+
+type RegExpDict = { 
+  Name: Lazy<RegExp> 
+  QName: Lazy<RegExp> 
+  PubidChar: Lazy<RegExp> 
+  InvalidChar_10: Lazy<RegExp> 
+  InvalidChar_11: Lazy<RegExp> 
+}
 
 /**
  * Represents an object implementing DOM algorithms.
@@ -17,6 +25,7 @@ export class DOMImpl {
   private _window: Window | null = null
   private _compareCache: CompareCache<Node>
   private _rangeList: ObjectCache<Range>
+  private _regExp: RegExpDict
 
   /**
    * Initializes a new instance of `DOM`.
@@ -24,6 +33,18 @@ export class DOMImpl {
   private constructor() {
     this._compareCache = new CompareCache<Node>()
     this._rangeList = new ObjectCache<Range>()
+    this._regExp = {
+      /** Matches a valid XML name */
+      Name: new Lazy<RegExp>(() => /^([:A-Z_a-z\xC0-\xD6\xD8-\xF6\xF8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])([\x2D\.0-:A-Z_a-z\xB7\xC0-\xD6\xD8-\xF6\xF8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])*$/),
+      /** Matches a valid XML qualified name */
+      QName: new Lazy<RegExp>(() => /^(([A-Z_a-z\xC0-\xD6\xD8-\xF6\xF8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])([\x2D\.0-9A-Z_a-z\xB7\xC0-\xD6\xD8-\xF6\xF8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])*:([A-Z_a-z\xC0-\xD6\xD8-\xF6\xF8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])([\x2D\.0-9A-Z_a-z\xB7\xC0-\xD6\xD8-\xF6\xF8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])*)|(([A-Z_a-z\xC0-\xD6\xD8-\xF6\xF8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])([\x2D\.0-9A-Z_a-z\xB7\xC0-\xD6\xD8-\xF6\xF8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])*)$/),
+      /** Matches a valid XML public identifier string */
+      PubidChar: new Lazy<RegExp>(() => /^[\x20\x0D\x0AA-Z-a-z0-9-'()+,./:=?;!*#@$_%]*$/),
+      /** Matches an invalid character according to XML 1.0 */
+      InvalidChar_10: new Lazy<RegExp>(() => /[\0-\x08\x0B\f\x0E-\x1F\uFFFE\uFFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]/),
+      /** Matches an invalid character according to XML 1.1 */
+      InvalidChar_11: new Lazy<RegExp>(() => /[\0\uFFFE\uFFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]/)
+    }
   }
 
   /**
@@ -71,6 +92,11 @@ export class DOMImpl {
    * Gets the global range list.
    */
   get rangeList(): ObjectCache<Range> { return this._rangeList }
+
+  /**
+   * Gets the global RegExp list.
+   */
+  get regExp(): RegExpDict { return this._regExp }
 
   /**
    * Returns the instance of `DOM`. 
