5c152dca53992e46939f6379f652a9f9cd2b2110
Ozgur Ozcitak <ozcitak@yahoo.com>
2020-01-03 16:08:42 +0300

    Optimize XML name check algorithm to check code points instead of regexp

--- src/algorithm/XMLAlgorithm.ts
+++ src/algorithm/XMLAlgorithm.ts
@@ -6,7 +6,46 @@ import { dom } from "../dom"
  * @param name - name string to test
  */
 export function xml_isName(name: string): boolean {
-  return dom.regExp.Name.value.test(name)
+  for (let i = 0; i < name.length; i++) {
+    const c1 = name.charCodeAt(i)
+    let n = c1
+    if (c1 >= 0xD800 && c1 <= 0xDBFF && i < name.length - 1) {
+      const c2 = name.charCodeAt(i + 1)
+      if (c2 >= 0xDC00 && c2 <= 0xDFFF) {
+        n = (c1 - 0xD800) * 0x400 + c2 - 0xDC00 + 0x10000 
+      }
+    }
+
+    // NameStartChar
+    if ((n >= 97 && n <= 122) || // [a-z]
+      (n >= 65 && n <= 90) || // [A-Z]
+      (n === 58 || n === 95 || // ':' or '_'
+      (n >= 0xC0 && n <= 0xD6) ||
+      (n >= 0xD8 && n <= 0xF6) ||
+      (n >= 0xF8 && n <= 0x2FF) ||
+      (n >= 0x370 && n <= 0x37D) ||
+      (n >= 0x37F && n <= 0x1FFF) ||
+      (n >= 0x200C && n <= 0x200D) ||
+      (n >= 0x2070 && n <= 0x218F) ||
+      (n >= 0x2C00 && n <= 0x2FEF) ||
+      (n >= 0x3001 && n <= 0xD7FF) ||
+      (n >= 0xF900 && n <= 0xFDCF) ||
+      (n >= 0xFDF0 && n <= 0xFFFD) ||
+      (n >= 0x10000 && n <= 0xEFFFF))) {
+      continue
+    } else if (i === 0) {
+      return false
+    } else if ((n === 45 || n === 46 || // '-' or '.'
+      (n >= 48 && n <= 57) || // [0-9]
+      (n === 0xB7) ||
+      (n >= 0x0300 && n <= 0x036F) ||
+      (n >= 0x203F && n <= 0x2040))) {
+      continue
+    } else {
+      return false
+    }
+  }
+  return true
 }
 
 /**
