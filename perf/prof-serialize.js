3b5d37b7977c56871f94d1e2c9204b5f5ba2adbb
Ozgur Ozcitak <ozcitak@yahoo.com>
2020-01-03 16:51:35 +0300

    Add serializer profile

---
+++ perf/prof-serialize.js
@@ -0,0 +1,21 @@
+const { DOMImplementation, XMLSerializer } = require("../lib")
+const { dom } = require("../lib/dom")
+
+dom.setFeatures(false)
+const impl = new DOMImplementation()
+const serializer = new XMLSerializer()
+for (let i = 0; i < 10000; i++) {
+  const doc = impl.createDocument(null, "", null)
+  const root = doc.createElement("root")
+  doc.appendChild(root)
+  for (let i = 0; i < 100; i++) {
+    const node = doc.createElement("node" + i.toString())
+    for (let j = 0; j < 10; j++) {
+      node.setAttribute("att" + j.toString(), "val" + j.toString())
+      const text = doc.createTextNode("text" + j.toString())
+      node.appendChild(text)
+    }
+    root.appendChild(node)
+  }
+  serializer.serializeToString(doc)
+}
