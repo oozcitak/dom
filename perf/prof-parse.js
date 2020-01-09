dc298ce819c69cc7b5420148ce4d2224dab1aaa2
Ozgur Ozcitak <ozcitak@yahoo.com>
2020-01-09 10:52:20 +0300

    Correct asset path and iterations in perf tests

--- perf/prof-parse.js
+++ perf/prof-parse.js
@@ -3,11 +3,11 @@ const { dom } = require("../lib/dom")
 const { readFileSync } = require("fs")
 const { join } = require("path")
 
-const sampleFileName = join(__dirname, "./assets/sample.xml")
+const sampleFileName = join(__dirname, "./assets/sample-2MB.xml")
 const xml = readFileSync(sampleFileName).toString()
 
 dom.setFeatures(false)
 const domParser = new DOMParser()
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 100; i++) {
   domParser.parseFromString(xml, "application/xml")
 }
