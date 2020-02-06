62d16d65d26a716f95549ba0f1172599251c1f4d
Ozgur Ozcitak <ozcitak@yahoo.com>
2020-02-06 13:12:00 +0300

    Test jsdom with node>=10 only

--- test/serializer/XMLSerializer.test.ts
+++ test/serializer/XMLSerializer.test.ts
@@ -1,5 +1,5 @@
 import $$ from "../TestHelpers"
-import { JSDOM } from "jsdom"
+import { gte } from 'semver'
 
 describe('XMLSerializer', () => {
 
@@ -411,39 +411,43 @@ describe('XMLSerializer', () => {
     expect(() => serializer._produceXMLSerialization(doc, true)).toThrow()
   })
 
-  test('serialize document from another implementation', () => {
-    const jsdomImpl = new JSDOM().window.document.implementation
-    const doc = jsdomImpl.createDocument(null, 'root', null)
-    const de = doc.documentElement
-    if (de) {
-      const node1 = doc.createElement('node')
-      node1.setAttribute('att', 'val')
-      de.appendChild(node1)
-      de.appendChild(doc.createComment('same node below'))
-      const node2 = doc.createElement('node')
-      node2.setAttribute('att', 'val')
-      node2.setAttribute('att2', 'val2')
-      de.appendChild(node2)
-      de.appendChild(doc.createProcessingInstruction('kidding', 'itwas="different"'))
-      de.appendChild(doc.createProcessingInstruction('for', 'real'))
-      de.appendChild(doc.createCDATASection('<greeting>Hello, world!</greeting>'))
-      const node3 = doc.createElement('text')
-      node3.appendChild(doc.createTextNode('alien\'s pinky toe'))
-      de.appendChild(node3)
-    }
+  if (gte(process.version, '10.0.0')) {
+    // only run if node version >=10 since this is the minimum requirement for jsdom
+    test('serialize document from another implementation', () => {
+      const { JSDOM } = require("jsdom")
+      const jsdomImpl = new JSDOM().window.document.implementation
+      const doc = jsdomImpl.createDocument(null, 'root', null)
+      const de = doc.documentElement
+      if (de) {
+        const node1 = doc.createElement('node')
+        node1.setAttribute('att', 'val')
+        de.appendChild(node1)
+        de.appendChild(doc.createComment('same node below'))
+        const node2 = doc.createElement('node')
+        node2.setAttribute('att', 'val')
+        node2.setAttribute('att2', 'val2')
+        de.appendChild(node2)
+        de.appendChild(doc.createProcessingInstruction('kidding', 'itwas="different"'))
+        de.appendChild(doc.createProcessingInstruction('for', 'real'))
+        de.appendChild(doc.createCDATASection('<greeting>Hello, world!</greeting>'))
+        const node3 = doc.createElement('text')
+        node3.appendChild(doc.createTextNode('alien\'s pinky toe'))
+        de.appendChild(node3)
+      }
 
-    const serializer = new $$.XMLSerializer()
-    expect(serializer.serializeToString(doc as any)).toBe(
-      '<root>' +
-      '<node att="val"/>' +
-      '<!--same node below-->' +
-      '<node att="val" att2="val2"/>' +
-      '<?kidding itwas="different"?>' +
-      '<?for real?>' +
-      '<![CDATA[<greeting>Hello, world!</greeting>]]>' +
-      '<text>alien\'s pinky toe</text>' +
-      '</root>'
-    )
-  })
+      const serializer = new $$.XMLSerializer()
+      expect(serializer.serializeToString(doc as any)).toBe(
+        '<root>' +
+        '<node att="val"/>' +
+        '<!--same node below-->' +
+        '<node att="val" att2="val2"/>' +
+        '<?kidding itwas="different"?>' +
+        '<?for real?>' +
+        '<![CDATA[<greeting>Hello, world!</greeting>]]>' +
+        '<text>alien\'s pinky toe</text>' +
+        '</root>'
+      )
+    })
+  }
 
 })
