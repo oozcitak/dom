b5afc349c96a8e6954010c9dad00e24347eb6b3b
Ozgur Ozcitak <ozcitak@yahoo.com>
2020-02-07 12:18:41 +0300

    Use abort API for AbortController test

--- test/dom/AbortController.test.ts
+++ test/dom/AbortController.test.ts
@@ -1,33 +1,60 @@
 import $$ from "../TestHelpers"
+import { AbortSignal } from "../../src/dom/interfaces"
+import { AbortError } from "../../src/dom/DOMException"
+import { abort_add } from "../../src/algorithm/AbortAlgorithm"
+
+// an example function using the abort API, from:
+// https://dom.spec.whatwg.org/#aborting-ongoing-activities-spec-example
+function doAmazingness(options: { signal: AbortSignal }) {
+  function importantWork() {
+    return new Promise(resolve => setTimeout(resolve, 100))
+  }
+
+  if (options.signal.aborted) {
+    return Promise.reject(new AbortError())
+  }
+
+  return new Promise<boolean>(async (resolve, reject) => {
+    let aborted = false
+
+    abort_add(() => {
+      aborted = true
+      reject(new AbortError())
+    }, options.signal)
+
+    for (let i = 0; i < 20; i++) {
+      if (aborted) break
+      // do important work
+      await importantWork()
+    }
+
+    const amazingResult = true
+    resolve(amazingResult)
+  })
+}
 
 describe('AbortController', () => {
 
-  const controller = new $$.AbortController()
-  const signal = controller.signal
+  test('abort()', (done) => {
+    const controller = new $$.AbortController()
+    const signal = controller.signal
 
-  function doWork(signal: any) {
-    if (signal.aborted) {
-      return Promise.reject(new Error('Aborted'))
-    }
-
-    return new Promise((resolve, reject) => {
-      signal.addEventListener('abort', () => {
-        reject(new Error('Aborted'))
-      })
-      for (let i = 0; i < 100000; i++) {
-        //
-      }
-      resolve(true)
-    })
-  }
-
-  test('abort()', () => {
     expect(signal.aborted).toBe(false)
-    doWork(signal)
+
+    doAmazingness({ signal }).then(
+      () => { throw new Error("Should have been aborted!") },
+      () => {
+        expect(signal.aborted).toBe(true)
+
+        // aborting twice has no effect
+        controller.abort()
+        expect(signal.aborted).toBe(true)
+    
+        done() 
+      }
+    )
+
     controller.abort()
-    expect(signal.aborted).toBe(true)
-    controller.abort()
-    expect(signal.aborted).toBe(true)
   })
 
 })
