1ec37e19cf2936ad056969e1fb944437719afa4e
oozcitak <oozcitak@gmail.com>
2021-07-29 09:50:10 +0300

    Add test for #7 also oozcitak/xmlbuilder2#88

---
+++ test/issues/007-xmlbuilder2-88.test.ts
@@ -0,0 +1,26 @@
+import $$ from "../TestHelpers"
+
+describe('Issues', () => {
+
+  test('DOM textContent returns encoded text #7', () => {
+    // https://github.com/oozcitak/xmlbuilder2/issues/88
+    const xmlStr = $$.t`
+    <?xml version="1.0" encoding="utf-8"?>
+    <root>
+      <text>&lt;data&gt;</text>
+      <text><![CDATA[<data>]]></text>
+    </root>
+    `
+    const parser = new $$.DOMParser()
+    const doc = parser.parseFromString(xmlStr, "application/xml")
+
+    expect($$.printTree(doc)).toBe($$.t`
+      root
+        text
+          # <data>
+        text
+          $ <data>
+      `)
+  })
+
+})
